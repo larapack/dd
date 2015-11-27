1d2e24f6d0a753bd1ce049891a27f38a5e5d8215
Mark Topper <mail@webman.io>
2015-11-27 06:43:27 -0500

    Applied fixes from StyleCI

--- src/helper.php
+++ src/helper.php
@@ -1,11 +1,10 @@
 <?php
 
-if (!function_exists('dd'))
-{
-  function dd()
-  {
-    $args = func_get_args();
-    call_user_func_array('dump', $args);
-    die();
-  }
+if (!function_exists('dd')) {
+    function dd()
+    {
+        $args = func_get_args();
+        call_user_func_array('dump', $args);
+        die();
+    }
 }
